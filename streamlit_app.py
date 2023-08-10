0b8860df50463333919a9afe76f12479c5a8be92
vivek l alex <47492781+viveklalex@users.noreply.github.com>
2023-08-11 03:29:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 streamlit.header('Breakfast Menu')
 streamlit.text('🍞🥑Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗🥣  Kale, Spinach & Rocket Smoothie')
