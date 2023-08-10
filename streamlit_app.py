ed2862167e22316398fc0ceb4dc95e1095c3a36e
vivek l alex <47492781+viveklalex@users.noreply.github.com>
2023-08-11 02:42:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 streamlit.header('Breakfast Menu')
 streamlit.text('🍞🥑Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗🥣  Kale, Spinach & Rocket Smoothie')
@@ -11,6 +12,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json()
 
 
 streamlit.dataframe(fruits_to_show)
