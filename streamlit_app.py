74f9cb885e443d15b574f679fc555e22e99ab7b0
vivek l alex <47492781+viveklalex@users.noreply.github.com>
2023-08-10 15:35:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.header('Breakfast Menu')
-streamlit.text(🍞🥑'Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍞🥑Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗🥣  Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg') 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
